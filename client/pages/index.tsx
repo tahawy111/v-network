946221ba0899ea3199910760bcb82c584a40dd2e
tahawy111 <amertahawy111@gmail.com>
2023-05-06 03:35:03 +0300

    add some changes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,12 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import Button from './components/ui/button';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className="">
-      <Button variant='outline-light' className=''>
-        Login
-      </Button>
+    <div>
+
     </div>
   );
 }
---
+++ client/pages/login.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+interface LoginProps {
+
+}
+
+const Login: FC<LoginProps> = ({ }) => {
+    return (<div>
+              <button className="btn-primary">
+        Login
+      </button>
+    </div>);
+};
+
+export default Login;
