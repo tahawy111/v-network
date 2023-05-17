14fd177c99a48387704beccabd7865fc20725521
tahawy111 <amertahawy111@gmail.com>
2023-05-18 00:01:13 +0300

    add register page

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -16,14 +16,18 @@ interface LoginProps {
 }
 
 const Login: FC<LoginProps> = ({ }) => {
-    const dispatch: AppDispatch = useDispatch();
     interface ILoginProps {
+        fullname: string;
         email: string;
         password: string;
+        cf_password: string;
     }
+    const dispatch: AppDispatch = useDispatch();
     const [formData, setFormData] = useState<ILoginProps>({
+        fullname: "",
         email: "",
-        password: ""
+        password: "",
+        cf_password: "",
     });
     const handleInputChange = ({ target }: InputChange) => {
         setFormData((v: any) => ({ ...formData, [target.name]: target.validity.valid ? target.value : v[target.name] }));
@@ -34,9 +38,10 @@ const Login: FC<LoginProps> = ({ }) => {
         if (!formData.email || !formData.password) return toast.error("Please fill in all fields.");
         if (!validEmail(formData.email)) return toast.error("Email isn't valid.");
         if (formData.password.length < 6) return toast.error("Password must be at least 6 chars.");
+        if (formData.password !== formData.cf_password) return toast.error("Password isn't match");
         dispatch(startLoading());
 
-        await dispatch(login(formData));
+        await dispatch(login({ fullname: formData.fullname, email: formData.email, password: formData.password }));
 
         dispatch(stopLoading());
     };
@@ -50,11 +55,13 @@ const Login: FC<LoginProps> = ({ }) => {
             <div className="border-2 max-w-sm p-5">
                 <h3 className="font-light text-center">V-NETWORK - Login</h3>
                 <form onSubmit={ handleSubmit }>
+                    <Input placeholder='Full Name' name='fullname' value={ formData.fullname } onChange={ handleInputChange } />
                     <Input placeholder='Email Address' name='email' value={ formData.email } onChange={ handleInputChange } />
                     <Input type='password' placeholder='Password' name='password' value={ formData.password } onChange={ handleInputChange } />
+                    <Input type='password' placeholder='Confirm Password' name='cf_password' value={ formData.cf_password } onChange={ handleInputChange } />
 
 
-                    <button className='btn-primary p-2 w-full'>Login</button>
+                    <button className='btn-primary p-2 w-full'>Register</button>
                 </form>
             </div>
         </div>
