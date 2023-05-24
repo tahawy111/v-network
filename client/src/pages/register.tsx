9220bef1f62be155829206d813b1cca018645e09
tahawy111 <amertahawy111@gmail.com>
2023-05-24 17:52:18 +0300

    Finish Success

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -18,16 +18,20 @@ interface LoginProps {
 const Login: FC<LoginProps> = ({ }) => {
     interface ILoginProps {
         fullname: string;
+        username: string;
         email: string;
         password: string;
         cf_password: string;
+        gender: string;
     }
     const dispatch: AppDispatch = useDispatch();
     const [formData, setFormData] = useState<ILoginProps>({
         fullname: "",
+        username: "",
         email: "",
         password: "",
         cf_password: "",
+        gender: ""
     });
     const handleInputChange = ({ target }: InputChange) => {
         setFormData((v: any) => ({ ...formData, [target.name]: target.validity.valid ? target.value : v[target.name] }));
@@ -41,7 +45,7 @@ const Login: FC<LoginProps> = ({ }) => {
         if (formData.password !== formData.cf_password) return toast.error("Password isn't match");
         dispatch(startLoading());
 
-        await dispatch(register({ fullname: formData.fullname, email: formData.email, password: formData.password }));
+        await dispatch(register({ fullname: formData.fullname, email: formData.email, password: formData.password, gender: formData.gender, username: formData.username }));
 
         dispatch(stopLoading());
     };
@@ -56,15 +60,16 @@ const Login: FC<LoginProps> = ({ }) => {
                 <h3 className="font-light text-center">V-NETWORK - Register</h3>
                 <form onSubmit={ handleSubmit }>
                     <Input placeholder='Full Name' name='fullname' value={ formData.fullname } onChange={ handleInputChange } />
-                    <Input placeholder='Email Address' name='email' value={ formData.email } onChange={ handleInputChange } />
+                    <Input placeholder='User Name' name='username' value={ formData.username } onChange={ handleInputChange } />
+                    <Input placeholder='Email Address' type='email' name='email' value={ formData.email } onChange={ handleInputChange } />
                     <Input type='password' placeholder='Password' name='password' value={ formData.password } onChange={ handleInputChange } />
                     <Input type='password' placeholder='Confirm Password' name='cf_password' value={ formData.cf_password } onChange={ handleInputChange } />
 
                     <div className='flex justify-between my-1'>
 
 
-                        <Input name='gender' placeholder='Male' type='radio' />
-                        <Input name='gender' placeholder='Female' type='radio' />
+                        <Input value={ formData.gender } onChange={ handleInputChange } name='gender' placeholder='Male' type='radio' />
+                        <Input value={ formData.gender } onChange={ handleInputChange } name='gender' placeholder='Female' type='radio' />
 
                     </div>
 
--- server/src/utils/valid.ts
+++ server/src/utils/valid.ts
@@ -1,5 +1,5 @@
 export function validEmail(email: string): boolean {
   const re: RegExp =
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  return re.test(String(email).toLowerCase());
+  return re.test(String(email));
 }
