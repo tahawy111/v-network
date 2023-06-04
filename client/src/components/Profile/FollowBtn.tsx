ff42b5fe223203a484dc368de77fb4a723c5d517
tahawy111 <amertahawy111@gmail.com>
2023-06-04 16:18:17 +0300

    .

--- client/src/components/Profile/FollowBtn.tsx
+++ client/src/components/Profile/FollowBtn.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 
 interface FollowBtnProps {
-  
+
 }
 
 export default function FollowBtn({ }: FollowBtnProps) {
-  return <button className="btn-outline-green">Follow</button>
+  const [followed, setFollowed] = useState<boolean>(false);
+  const handleUnFollow = () => {
+    setFollowed(false)
+   };
+  const handleFollow = () => { 
+    setFollowed(true)
+  };
+  return (<>
+    { followed ? (<>
+      <button onClick={ handleUnFollow } className="btn-outline-green">Follow</button>
+    </>) : (<>
+      <button onClick={ handleFollow } className="btn-outline-red">UnFollow</button>
+    </>) }
+  </>);
 }
