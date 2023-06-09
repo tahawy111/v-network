c8b768d42ee4d5a848fda20cb9749ce01d848ef5
tahawy111 <amertahawy111@gmail.com>
2023-06-10 02:48:16 +0300

    Create Posts [2]

--- client/src/components/Home/StatusModal.tsx
+++ client/src/components/Home/StatusModal.tsx
@@ -1,6 +1,6 @@
 import { setStatusModalShow } from '@/features/global/global';
 import { AppDispatch, RootState } from '@/features/store';
-import { ChangeEvent, useId, useState } from 'react';
+import { ChangeEvent, useId, useRef, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -12,8 +12,12 @@ export default function StatusModal({ }: StatusModalProps) {
     const dispatch: AppDispatch = useDispatch();
     const { auth } = useSelector((state: RootState) => state);
     const [content, setContent] = useState<string>("");
-    const [images, setImages] = useState<File[]>([]);
+    const [images, setImages] = useState<(File | { camera: string; })[]>([]);
+    const [stream, setStream] = useState<boolean>(false);
+    const [tracks, setTracks] = useState<MediaStreamTrack>();
     const fileId = useId();
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
     const handleChangeImages = (e: ChangeEvent<HTMLInputElement>) => {
         const files = Array.from(e.target.files!);
         let err = "";
@@ -32,6 +36,36 @@ export default function StatusModal({ }: StatusModalProps) {
         if (err) toast.error(err);
         setImages(prev => [...prev, ...newImages]);
     };
+    const deleteImage = (index: number) => {
+        const editedImages = [...images];
+        editedImages.splice(index, 1);
+        setImages(editedImages);
+    };
+    const handleStream = () => {
+        setStream(true);
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then((mediaStream) => {
+
+                    if (videoRef.current) videoRef.current.srcObject = mediaStream;
+                    videoRef.current?.play();
+                    const track = mediaStream.getTracks();
+                    setTracks(track[0]);
+                }).catch(err => toast.error(err));
+        }
+    };
+    const handleCapture = () => {
+        if (!videoRef.current || !canvasRef.current) return;
+        const width = videoRef.current.clientWidth;
+        const height = videoRef.current.clientHeight;
+        canvasRef.current?.setAttribute("height", `${height}`);
+        canvasRef.current?.setAttribute("width", `${width}`);
+        const ctx = canvasRef.current.getContext(`2d`);
+        if (!ctx) return;
+        ctx.drawImage(videoRef.current, 0, 0, width, height);
+        let URL = canvasRef.current.toDataURL();
+        setImages(prev => [...prev, { camera: URL }]);
+    };
 
     return <div className='fixed top-0 left-0 bg-black/50 w-full h-screen overflow-auto'>
         <form className="max-w-md w-full bg-white dark:bg-main flex flex-col gap-y-2 mx-auto my-8 p-5 rounded-md">
@@ -44,36 +78,53 @@ export default function StatusModal({ }: StatusModalProps) {
             <div className="">
                 <textarea rows={ 5 } className='outline-none border-none resize-none' name="content" value={ content } onChange={ (e) => setContent(e.target.value) } placeholder={ `${auth.user?.username}, What are you thinking?` } />
 
-                <div className="max-h-64 w-full overflow-y-auto grid grid-cols-3 gap-3 place-items-center">
                 {
-                    images.map((img, index) => (
-                        <div className='w-full h-full relative' key={ index }>
-                            <img src={ URL.createObjectURL(img) } alt="images" className='w-full h-full block object-contain border-gray-200 border p-1 rounded-md' />
-                            <span className='cursor-pointer text-xl font-bold absolute top-1 right-1 bg-white border border-red-500 text-red-500 rounded-full px-2 py-0.5 transition-colors duration-150 hover:bg-red-500 hover:text-white hover:border-white pt-0 ease-out'>&times;</span>
+                    stream && (
+                        <div className='relative'>
+                            <video src="" autoPlay muted ref={ videoRef } className='w-full h-full' />
+                            <span onClick={() => setStream(false)} className='cursor-pointer text-red-500 absolute bg-white top-1 right-1 text-xl font-bold rounded-full px-2 border border-red-500 pt-0 pb-0.5'>&times;</span>
+                            <canvas className={`hidden`} ref={ canvasRef } />
                         </div>
-                    ))
+                    )
                 }
+
+                <div className="max-h-64 w-full overflow-y-auto grid grid-cols-3 gap-3 place-items-center">
+                    {
+                        images.map((img, index) => (
+                            <div className='w-full h-full relative' key={ index }>
+                                <img src={ img instanceof File ? URL.createObjectURL(img) : img.camera } alt="images" className='w-full h-full block object-contain border-gray-200 border p-1 rounded-md' />
+                                <span onClick={ () => deleteImage(index) } className='cursor-pointer text-xl font-bold absolute top-1 right-1 bg-white border border-red-500 text-red-500 rounded-full px-2 py-0.5 transition-colors duration-150 hover:bg-red-500 hover:text-white hover:border-white pt-0 ease-out'>&times;</span>
+                            </div>
+                        ))
+                    }
                 </div>
 
                 <div className="flex relative justify-center items-center">
-
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="w-8 h-8 cursor-pointer">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
-                    </svg>
-
-                    <div className="flex items-center overflow-hidden mx-2 relative">
-                        <label htmlFor={ fileId }>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="w-8 h-8 cursor-pointer">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    {
+                        stream
+                            ? <svg onClick={ handleCapture } xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="w-8 h-8 cursor-pointer">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
                             </svg>
-                        </label>
+                            : <>
+                                <svg onClick={ handleStream } xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="w-8 h-8 cursor-pointer">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                                </svg>
+                            </>
+                    }
 
+                                <div className="flex items-center overflow-hidden mx-2 relative">
+                                    <label htmlFor={ fileId }>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="w-8 h-8 cursor-pointer dark:text-white">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                                        </svg>
+                                    </label>
 
-                        <input className='hidden' type="file" name='file' id={ fileId } multiple accept='image/*' onChange={ handleChangeImages } />
+                                    <input className='hidden' type="file" name='file' id={ fileId } multiple accept='image/*' onChange={ handleChangeImages } />
 
+                                </div>
 
-                    </div>
                 </div>
             </div>
 
