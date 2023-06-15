fe4de322773babf13ddff46608c98ac87f2516f9
tahawy111 <amertahawy111@gmail.com>
2023-06-15 17:13:47 +0300

    .

--- server/src/models/Comment.ts
+++ server/src/models/Comment.ts
@@ -2,16 +2,25 @@ import { Schema, model } from 'mongoose';
 
 // 1. Create an interface representing a document in MongoDB.
 export interface IComment {
-    _id: string;
+    _id: Schema.Types.ObjectId;
     content: string;
     tag: object;
-    
+    reply: Schema.Types.ObjectId;
+    likes: Schema.Types.ObjectId[];
+    user: Schema.Types.ObjectId;
+    postId: Schema.Types.ObjectId;
+    postUserId: Schema.Types.ObjectId;
 }
 
 // 2. Create a Schema corresponding to the document interface.
 const commentSchema = new Schema<IComment>({
     content: { type: String, required: true },
-    tag: Object
+    tag: Object,
+    reply: Schema.Types.ObjectId,
+    likes: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+    user: { type: Schema.Types.ObjectId, ref: 'user' },
+    postId: Schema.Types.ObjectId,
+    postUserId: Schema.Types.ObjectId
 }, {
     timestamps: true
 });
